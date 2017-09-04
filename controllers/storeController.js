d8698ceb9df0648b77464483a7404780d88d5737
kephin <wunshon76325@gmail.com>
2017-09-05 01:35:43 +0800

    add GET /object/:key route for timestamp query

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const Store = require('../models/Store');
 
 exports.getStore = async(req, res) => {
+  const queryTimeStamp = parseInt(req.query.timestamp, 10);
   try {
-    const store = await Store
+    const store = queryTimeStamp ?
+      await Store
+      .find({ key: req.params.key })
+      .where('timeStamp').lte(queryTimeStamp)
+      .sort({ timeStamp: -1 })
+      .limit(1) :
+      await Store
       .find({ key: req.params.key })
       .sort({ timeStamp: -1 })
       .limit(1);
