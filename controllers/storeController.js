8380c0801d79af20682ec6459b826155025afe66
kephin <wunshon76325@gmail.com>
2017-09-05 01:34:03 +0800

    add GET /object/:key route

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const Store = require('../models/Store');
 
+exports.getStore = async(req, res) => {
+  try {
+    const store = await Store
+      .find({ key: req.params.key })
+      .sort({ timeStamp: -1 })
+      .limit(1);
+    const { value } = store[0];
+    res.json({ value });
+  } catch (err) {
+    res.status(404).json(err);
+  }
+};
+
 exports.addStore = async(req, res) => {
   const [key, value] = Object.entries(req.body)[0];
   try {
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const storeController = require('../controllers/storeController');
 
+router.get('/:key', storeController.getStore);
 router.post('/', storeController.addStore);
 
 module.exports = router;
