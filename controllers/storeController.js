121344dc303ec8ebac76df4bdcea62d2c9992022
kephin <wunshon76325@gmail.com>
2017-09-06 03:49:48 +0800

    update getStore controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,22 +1,43 @@
 const Store = require('../models/Store');
 
 exports.getStore = async(req, res, next) => {
-  const queryTimeStamp = parseInt(req.query.timestamp, 10);
-  try {
-    const store = queryTimeStamp ?
-      await Store
-      .find({ key: req.params.key })
-      .where('timeStamp').lte(queryTimeStamp)
-      .sort({ timeStamp: -1 })
-      .limit(1) :
-      await Store
-      .find({ key: req.params.key })
-      .sort({ timeStamp: -1 })
-      .limit(1);
-    const { value } = store[0];
-    res.json({ value });
-  } catch (err) {
-    next(err);
+  const queryLength = Object.keys(req.query).length;
+  switch (queryLength) {
+    case 0:
+      // no query will return latest value
+      try {
+        const store = await Store
+          .find({ key: req.params.key })
+          .sort({ timeStamp: -1 })
+          .limit(1);
+        const { value } = store[0];
+        res.json({ value });
+      } catch (err) {
+        next(new Error(`${req.params.key} not found!`));
+      }
+      break;
+    case 1:
+      // check if the key is correct
+      if (Object.keys(req.query)[0] !== 'timestamp') return next(new Error('Bad request with incorrect query name.'));
+      // check if the value is a number
+      const queryString = Object.values(req.query)[0];
+      const queryTimeStamp = Number(queryString);
+      if (!queryString || isNaN(queryTimeStamp)) return next(new Error('Bad request with incorrect query value.'));
+      // return value within the timestamp query
+      try {
+        const store = await Store
+          .find({ key: req.params.key })
+          .where('timeStamp').lte(queryTimeStamp)
+          .sort({ timeStamp: -1 })
+          .limit(1);
+        const { value } = store[0];
+        res.json({ value });
+      } catch (err) {
+        next(new Error(`${req.params.key} not found!`));
+      }
+      break;
+    default: // more than 1 queries
+      next(new Error('Bad request with too much queries.'));
   }
 };
 
